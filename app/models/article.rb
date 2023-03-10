01a668faacb7e98981b74b8d0fa92945a89c3409
Ronak Gupta <ronak.gupta@deeporion.com>
2023-03-10 18:33:38 +0530

    CallBacks Learned

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,16 @@
 class Article < ApplicationRecord
-    has_many :comments
-    validates :title,presence: true , uniqueness: true
-    validates :body, presence: true , length: {minimum: 8}
 
-    
+  #Association
+  has_many :comments
+
+  #Validation
+  validates :title,presence: true , uniqueness: true
+  validates :body, presence: true , length: {minimum: 8}
+
+  #CallBack
+  after_destroy :log_destroy_action
+
+  def log_destroy_action
+    puts 'Article destroyed'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_173004) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_10_104357) do
   create_table "accounts", force: :cascade do |t|
     t.string "subdomain"
     t.datetime "created_at", null: false
@@ -97,6 +97,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_173004) do
     t.datetime "end_date", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "comments", "articles"
